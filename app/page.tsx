a14a6d74941335f7721d8093d6951e87c374cf30
RedPanda-08 <navrajkalsi2005@gmail.com>
2025-12-03 22:12:22 +0530

    Added Calendar feature

--- app/page.tsx
+++ app/page.tsx
@@ -211,18 +211,6 @@ export default function HomePage() {
             </div>
           </motion.div>
         </motion.div>
-
-        {/* Scroll Indicator */}
-        <motion.div 
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 1 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce"
-        >
-          <div className="w-6 h-10 border-2 border-white/20 rounded-full flex justify-center p-2">
-            <div className="w-1.5 h-3 bg-white/40 rounded-full"></div>
-          </div>
-        </motion.div>
       </section>
 
       {/* Stats Section */}
